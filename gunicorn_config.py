518da7c13ab88f9ff82b501c28ebe483c58e4c9a
dtynn <dtynn@163.com>
2015-06-24 09:10:06 +0800

    gunicorn things

---
+++ gunicorn_config.py
@@ -0,0 +1,5 @@
+bind = "127.0.0.1:5000"
+workers = 4
+accesslog = "/var/log/gunicorn/pyco.blog.access.log"
+errorlog = "/var/log/gunicorn/pyco.blog.error.log"
+pidfile = "/var/log/gunicorn/pyco.blog.pid"
